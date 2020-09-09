6a64b487bb9fb4716c478ff09985bb34a75c1b24
Domin0x <kozlowski.dominik.mail@gmail.com>
2020-09-10 00:16:08 +0200

    use @Resource in place of @Autowired

--- src/main/java/com/domin0x/NBARadars/radar/RadarLayoutService.java
+++ src/main/java/com/domin0x/NBARadars/radar/RadarLayoutService.java
@@ -1,5 +1,6 @@
 package com.domin0x.NBARadars.radar;
 
+import com.domin0x.NBARadars.stats.IStat;
 import com.domin0x.NBARadars.stats.PerGameStats;
 import com.domin0x.NBARadars.stats.PerGameStatsService;
 import com.domin0x.NBARadars.player.Player;
@@ -10,17 +11,18 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import java.util.List;
 import java.util.Map;
 
 @Service
 public class RadarLayoutService {
 
-    @Autowired
+    @Resource
     private Map<RadarType, RadarLayout> radarTemplatesMap;
 
-    @Autowired
-    private Map<RadarType, List<StatType>> radarStatTypeMap;
+    @Resource
+    private Map<RadarType, List<StatType>> radarStatTypesMap;
 
     @Autowired
     private PerGameStatsService perGameStatsService;
@@ -46,10 +48,10 @@ public class RadarLayoutService {
         return new RadarLayout(radarTemplatesMap.get(templateKey));
     }
 
-    private void fillLayoutData(RadarLayout layout, PerGameStats stats){
+    private void fillLayoutData(RadarLayout layout, IStat stats){
         List<Category<Number>> categories = layout.getCategories();
         int i = 0;
-        for(StatType statType : radarStatTypeMap.get(layout.getType())){
+        for(StatType statType : radarStatTypesMap.get(layout.getType())){
             categories.get(i).setValue(statType.getStatValue(stats));
             i++;
         }
